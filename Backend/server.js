5605a41fcf91c2ff057a7e44ba99a3d8f9323fd7
Manjunath063 <manjunathjangam198@gmail.com>
2025-07-16 15:37:20 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -114,5 +114,5 @@ app.get('/api/payslips/:empCode', async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running on http://3.85.61.23:${port}`);
+    console.log(`Server running on http://13.61.2.118:${port}`);
 });
